b0336a8ab3d2edab11d01b58b879ea03da4d13bc
Profsoul <profsoul23@gmail.com>
2026-01-01 13:18:56 +0530

    Updated Server to 443

--- prodction_tracker_V4.py
+++ prodction_tracker_V4.py
@@ -414,4 +414,4 @@ def download_csv(n):
 
 # ---------- RUN ----------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8050,debug=False)
+    app.run(host="0.0.0.0",port=443,debug=False)
